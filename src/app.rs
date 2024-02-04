b9c8c4eb35dcfe54bb77b1c1e2ce8493552750b9
BrotherDan Boyce <danboyce@sonic.net>
2024-02-04 14:14:37 -0800

    Minor display changes

--- src/app.rs
+++ src/app.rs
@@ -192,14 +192,14 @@ impl eframe::App for Populator {
                 //ui.label(format!("= {}", self.settings.equation_settings.output));
 
                 ui.label(
-                    RichText::new("  Results  :  " )
+                    RichText::new("  Results:  " )
                         .size(15.0)
                         .color(Color32::WHITE)
                         .background_color(Color32::from_rgb(0, 0, 0)),
                 );
 
                 ui.label(
-                    RichText::new(format!("= {}", self.settings.equation_settings.output))
+                    RichText::new(format!(" = {} ", self.settings.equation_settings.output))
                         .size(15.0)
                         .color(Color32::WHITE)
                         .background_color(Color32::from_rgb(0, 0, 0)),

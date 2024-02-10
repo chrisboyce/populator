975ad98ffd13cf2723f2cb85e289710b8deecb84
BrotherDan Boyce <danboyce@sonic.net>
2024-02-10 14:40:47 -0800

    Changes to position of egui and eframe links

--- src/app.rs
+++ src/app.rs
@@ -202,8 +202,8 @@ impl eframe::App for Populator {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.spacing_mut().item_spacing.y = 20.0;
+            
+            
 
             ui.heading(
                 RichText::new("                     SnArKn Calculator                      ")
@@ -211,8 +211,21 @@ impl eframe::App for Populator {
                     .size(20.0), // .color(Color32::WHITE)
                                  // .background_color(Color32::from_rgb(0, 0, 0)),
             );
-
-            
+            ui.spacing_mut().item_spacing.y = 20.0;
+            {
+                ui.horizontal(|ui| {
+                    ui.spacing_mut().item_spacing.x = 0.0;
+                    ui.label("                         (Powered by ");
+                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+                    ui.label(" and ");
+                    ui.hyperlink_to(
+                        "eframe",
+                        "https://github.com/emilk/egui/tree/master/crates/eframe",
+                    );
+                    ui.label(" )                        ");
+                });
+            }
+            ui.spacing_mut().item_spacing.y = 10.0;
 
             ui.label(
                 RichText::new("  Enter Expression:  ").size(15.0), 
@@ -250,8 +263,8 @@ impl eframe::App for Populator {
                 });
             });
 
-            ui.spacing_mut().item_spacing.y = 50.0;
-            {
+            //ui.spacing_mut().item_spacing.y = 100.0;
+           /* {
                 ui.horizontal(|ui| {
                     ui.spacing_mut().item_spacing.x = 0.0;
                     ui.label("Powered by ");
@@ -263,7 +276,7 @@ impl eframe::App for Populator {
                     );
                     ui.label(".");
                 });
-            }
+            }*/
 
             if self.settings.equation_settings.show_keypad {
                 ui.separator();
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() -> eframe::Result<()> {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
+            .with_inner_size([400.0, 500.0])
             .with_min_inner_size([300.0, 220.0])
             .with_icon(
                 // NOE: Adding an icon is optional
